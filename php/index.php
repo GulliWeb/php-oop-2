bc211dabc4581b493b6dca795a28535792e5aa6d
Andrea Gulli <andreagulli11@gmail.com>
2024-10-10 15:36:19 +0200

    add project files & php classes

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+// Importo i file delle mie classi nel mio index PHP
+require_once __DIR__ ."/../classes/Ecommerce.php"
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Css -->
+    <link rel="stylesheet" href="styles/style.css">
+    <!-- Bootstrap -->
+     <link rel="stylesheet" href=" 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>PHP OOP 2</title>
+</head>
+<body>
+        
+</body>
+</html>
