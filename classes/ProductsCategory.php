f8e2723e441c2fb4f9a187509d89747f40056502
Andrea Gulli <andreagulli11@gmail.com>
2024-10-10 17:19:45 +0200

    add methods getter & setter to productsType & productsCategory classes

--- classes/ProductsCategory.php
+++ classes/ProductsCategory.php
@@ -1,6 +1,20 @@
 <?php
-class ProductsCategory extends Products {
-    private $categoryId, $categoryName, $description;
-}
+class ProductsCategory {
+    private $name; // Nome della categoria
 
+    // Metodo Costruttore
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    // Metodo Getter
+    public function getName() {
+        return $this->name;
+    }
+
+    // Metodo Setter
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
 ?>
--- classes/ProductsType.php
+++ classes/ProductsType.php
@@ -1,6 +1,20 @@
 <?php
-class ProductsType extends ProductsCategory{
-    private $typeId, $typeName, $categoryId, $description;
+class ProductsType {
+    private $type;
+
+    // Metodo costruttore
+    public function __construct($type) {
+        $this->type = $type;
+    }
+
+    // Metodo getter
+    public function getType() {
+        return $this->type;
+    }
+
+    // Metodo setter
+    public function setType($type) {
+        $this->type = $type;
+    }
 }
 ?>
-
